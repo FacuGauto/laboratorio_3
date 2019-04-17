21b3b447a7dd2891caa9c1e26417fd2584668559
Your Name <you@example.com>
2019-04-16 22:14:30 -0300

    js.2

--- JS/ej1/script.js
+++ JS/ej1/script.js
@@ -1,5 +1,20 @@
-function Saludar(){
+//function Saludar(){
     //alert("Hola Mundo");
-    document.getElementById("p1").innerHTML = "Esto es un parrafo";
-}
+  //  document.getElementById("p1").innerHTML = "Esto es un parrafo";
+//}
+//document.getElementById('p1').onclick =function(){
+//    this.innerHTML = "AHORA";
+//}
 
+var miParrafo;
+window.addEventListener('load', inicializarEventos);
+
+function inicializarEventos(){
+    miParrafo = document.getElementById('p1');
+    miParrafo.addEventListener('click',function(){
+        this.innerHTML = "Hola";
+    })
+}
+//document.getElementById('p1').addEventListener('click', function(){
+//    this.innerHTML = "dsdsdsdsd";
+//});
